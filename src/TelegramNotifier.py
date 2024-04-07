d34f9ca2e6bb4fd2dfb685c058e48ce899cd97f8
Nikolas <childcity0@gmail.com>
2024-04-07 03:34:59 +0300

    edit_message

--- src/TelegramNotifier.py
+++ src/TelegramNotifier.py
@@ -5,154 +5,165 @@ from telethon import TelegramClient
 from telethon.hints import EntityLike
 
 def read_config():
-    import json
-    
-    with open('src/TelegramNotifier.json', 'r') as file:
-        return json.load(file)
+	import json
+	
+	with open('src/TelegramNotifier.json', 'r', encoding='utf-8') as file:
+		return json.load(file)
 
 async def CreateTelegaApiClient() -> TelegramClient:
-    config = read_config()
+	config = read_config()
 
-    api_id = config['api_id']
-    api_hash = config['api_hash']
-    phone = config['phone']
+	api_id = config['api_id']
+	api_hash = config['api_hash']
+	phone = config['phone']
 
-    client = TelegramClient('session', api_id, api_hash)
+	client = TelegramClient('session', api_id, api_hash)
 
-    # connecting and building the session
-    await client.connect()
+	# connecting and building the session
+	await client.connect()
 
-    # in case of script ran first time it will
-    # ask either to input token or otp sent to
-    # number or sent or your telegram id
-    if not await client.is_user_authorized():
-        from telethon.errors import SessionPasswordNeededError
+	# in case of script ran first time it will
+	# ask either to input token or otp sent to
+	# number or sent or your telegram id
+	if not await client.is_user_authorized():
+		from telethon.errors import SessionPasswordNeededError
 
-        try:
-            await client.send_code_request(phone)
-            # signing in the client
-            await client.sign_in(phone=phone, code=input('Enter the code: '))
-        except SessionPasswordNeededError:
-            import getpass
-            await client.sign_in(password=getpass.getpass(prompt='Enter the password:'))
-        except Exception as e:
-            print('Exception', e)
+		try:
+			await client.send_code_request(phone)
+			# signing in the client
+			await client.sign_in(phone=phone, code=input('Enter the code: '))
+		except SessionPasswordNeededError:
+			import getpass
+			await client.sign_in(password=getpass.getpass(prompt='Enter the password:'))
+		except Exception as e:
+			print('Exception', e)
 
-    return client if client.is_connected() else None
+	return client if client.is_connected() else None
 
 
 async def DestroyTelegaApiClient(client: TelegramClient):
-    await client.disconnect()
+	await client.disconnect()
 
 
 def DoCheck() -> list:
-    import requests
-    import regex as re
+	import requests
+	import regex as re
 
-    url = 'http://194.44.37.173/certificates.html'
+	url = 'http://194.44.37.173/certificates.html'
 
-    resp = requests.get(url=url)
-    resp.encoding = 'utf-8'
+	resp = requests.get(url=url)
+	resp.encoding = 'utf-8'
 
-    # Finding what I want
-    founded:list = re.findall('[гГ]ородец', resp.text)
+	# Finding what I want
+	founded:list = re.findall('[гГ]ородец', resp.text)
 
-    return founded
+	return founded
 
 
 async def NotifyMe(client: TelegramClient, message: str):
-    print(message)
+	print(message)
 
-    try:
-        receiver = await client.get_input_entity('@skulazkiy')
+	try:
+		receiver = await client.get_input_entity('@skulazkiy')
 
-        # sending message using telegram client
-        await client.send_message(receiver, message, parse_mode='html')
-    except Exception as e:
-        print(e)
+		# sending message using telegram client
+		await client.send_message(receiver, message, parse_mode='html')
+	except Exception as e:
+		print(e)
 
-    return
+	return
 
 
 async def StartChecker():
-    import time
-    import datetime
+	import time
+	import datetime
 
-    client:TelegramClient = None
+	client:TelegramClient = None
 
-    try:
-        client = await CreateTelegaApiClient()
+	try:
+		client = await CreateTelegaApiClient()
 
-        if not (client):
-            print('Client not connected!')
-            return
+		if not (client):
+			print('Client not connected!')
+			return
 
-        while (True):
-            resp:list = DoCheck()
+		while (True):
+			resp:list = DoCheck()
 
-            if resp:
-                await NotifyMe(client=client, message=str(resp))
-                return
-            else:
-                print(str(datetime.datetime.now()) + ': Nothing...')
+			if resp:
+				await NotifyMe(client=client, message=str(resp))
+				return
+			else:
+				print(str(datetime.datetime.now()) + ': Nothing...')
 
-            time.sleep(60)
+			time.sleep(60)
 
-    finally:
-        if (client):
-            await DestroyTelegaApiClient(client)
+	finally:
+		if (client):
+			await DestroyTelegaApiClient(client)
 
-    return
+	return
 
 
 async def UpdateChannelMasseges():
-    import time
-    import datetime
-    from telethon import hints
-    from telethon.client import messages
+	import time
+	import datetime
+	from telethon import hints
+	from telethon.client import messages
+	from telethon.errors import MessageNotModifiedError
 
-    client:TelegramClient = None
+	client:TelegramClient = None
 
-    try:
-        client = await CreateTelegaApiClient()
+	try:
+		client = await CreateTelegaApiClient()
 
-        if not client:
-            print('Client not connected!')
-            return
+		if not client:
+			print('Client not connected!')
+			return
 
-        cfg = read_config()
-        chat_id = cfg['chat_id']
-        channel_fullname = cfg['channel_fullname']
-        channel_link = cfg['channel_link']
-        
-        async for msg in client.iter_messages(entity=chat_id, limit=10):
-            if ('' in msg.message):
-                new_msg: str = msg.message.replace(channel_fullname,
-                    f'hh[{channel_fullname}]({channel_link})')
-                
-                await client.edit_message(chat_id, msg.id, new_msg)
-                print(msg.message)
-                print('------------------------')
+		cfg = read_config()
+		chat_id = cfg['chat_id']
+		channel_fullname = cfg['channel_fullname']
+		channel_link = cfg['channel_link']
+		
+		async for msg in client.iter_messages(entity=chat_id, limit=100):
+			if (channel_fullname in msg.message):
+				new_msg: str = msg.message.replace(channel_fullname,
+					f'[{channel_fullname}]({channel_link})')
 
-        
-    except Exception as e:
-        print('Exception', e)
-    finally:
-        if client:
-            await DestroyTelegaApiClient(client)
+				print('OLD', msg.message)
+				print('NEW', new_msg)
+				print('------------------------')
+				try:
+					await client.edit_message(
+						entity=chat_id, 
+						message=msg.id, 
+						text=new_msg,
+						link_preview=False
+						)
+				except MessageNotModifiedError as e:
+					# API throw an exception if message was 'the same'
+					if 'Content of the message was not modified' not in e.args[0]:
+						raise e
 
-    return
+	except Exception as e:
+		print('Exception\n', e)
+	finally:
+		if client:
+			await DestroyTelegaApiClient(client)
+
+	return
 
 
 if __name__ == '__main__':
-    import asyncio
+	import asyncio
 
-    print('Started')
+	print('Started')
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(
-        # DoCheck()
-        UpdateChannelMasseges()
-    )
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(
+		# DoCheck()
+		UpdateChannelMasseges()
+	)
 
-    print('Exited')
+	print('Exited')
